374380979ec9b216255bcf933ef042e5746b69ed
Miguel Acevedo <admin@miguelacevedo.com>
2023-05-07 20:41:24 -0700

    Removes unused JSX
    Cleans up Toggle.jsx by removing unused commented out code.

--- src/components/Toggle/Toggle.jsx
+++ src/components/Toggle/Toggle.jsx
@@ -3,11 +3,6 @@ import "./Toggle.css";
 
 const Toggle = ({ onChange }) => {
     return (
-        // <InputWrapper value="Numbers?">
-        //     <span>Numbers?</span>
-        //     <Input type="checkbox" name="useNumbers" onChange={onChange} />
-        //     <Slider />
-        // </InputWrapper>
 
         <label>
             <span>Numbers?</span>
